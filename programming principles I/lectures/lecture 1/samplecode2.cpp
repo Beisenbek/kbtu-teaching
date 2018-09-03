7e234f64213aac3ac012658f12c47067febda336
bsnbk <beysenbek@gmail.com>
2018-09-03 09:27:32 +0600

    upd

---
+++ programming principles I/lectures/lecture 1/samplecode2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+
+    //print some text to the console
+    cout << "hello world!";
+
+    /*
+    some multiline comment
+    some multiline comment
+    */
+
+
+    return 0;
+}
